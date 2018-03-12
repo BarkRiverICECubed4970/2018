5e0ef515d7a04ba1d194016cc2f4248fc53e0838
asgaardc <asgaardc@gmail.com>
2018-03-12 12:03:24 -0400

    lock and unlock winch

--- src/org/usfirst/frc/team4970/robot/subsystems/ClimbMotor.java
+++ src/org/usfirst/frc/team4970/robot/subsystems/ClimbMotor.java
@@ -7,13 +7,44 @@ import org.usfirst.frc.team4970.robot.commands.OperateWinch;
 
 public class ClimbMotor extends Subsystem {
 
+    public enum SolenoidState
+    {
+	WINCH_LOCKED, WINCH_UNLOCKED
+    };
+	
+    private static SolenoidState _solenoidState = SolenoidState.WINCH_LOCKED;
+	
+    WPI_TalonSRX m_solenoid = new WPI_TalonSRX(Constants.solenoidMotorCanAddress);
+
     public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		setDefaultCommand(new OperateWinch());
     }
     
-    public void operateWinch() {
-    	Robot.m_climber.set(Robot.m_oi.joystick.getRawAxis(5));
+    public void lockWinch()
+    {
+	m_soloenoid.set(1.0);
+	_solenoidState = SolenoidState.WINCH_LOCKED);
+    }
+	
+    public void unlockWinch()
+    {
+	m_soloenoid.set(0.0);
+	timerdelay(halfsecond);
+	_solenoidState = SolenoidState.WINCH_UNLOCKED);
+    }
+	
+    public void extendWinch(double dutyCycle) {
+	if (_solenoidState == SolenoidState.WINCH_LOCKED)
+	{
+	    unlockWinch();
+	}
+	Robot.m_climber.set(dutyCycle);
+    }
+    
+    public void reelWinch(double dutyCycle) {
+	lockWinch();
+	Robot.m_climber.set(dutyCycle);
     }
     
     public void stop() {
